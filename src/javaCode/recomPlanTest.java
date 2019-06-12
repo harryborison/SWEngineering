66c24208f77e764e5f59c27b334bb78b9074e4ce
harryborison <33567924+harryborison@users.noreply.github.com>
2019-06-13 00:16:14 +0900

    recomplan Test코드
    recomplan Test

--- src/javaCode/recomPlanTest.java
+++ src/javaCode/recomPlanTest.java
@@ -20,6 +20,8 @@ public class recomPlanTest {
 	public void testMatchtelecom() {
 		recomPlan p = new recomPlan();
 		String[] wanttel = new String[3];
+		String[] wanttel2 = new String[1];
+		wanttel[0]= "NULL";
 		wanttel[0] = "SK";
 		wanttel[1] = "KT";
 		wanttel[2] = "LG U+";
@@ -29,13 +31,43 @@ public class recomPlanTest {
 		assertEquals(3,p.matchtelecom("LG U+", wanttel));
 		assertEquals(0,p.matchtelecom("SKT", wanttel));
 		assertEquals(0,p.matchtelecom("LGU+", wanttel));
+		assertEquals(0,p.matchtelecom("LG U+", wanttel2));
+		assertEquals(0,p.matchtelecom("SK", wanttel2));
+		assertEquals(0,p.matchtelecom("KT", wanttel2));
 		
 		
 	}
 
 	@Test
 	public void testMatchtype() {
-	
+		recomPlan p = new recomPlan();
+		String[] wanttype = new String[2];
+		wanttype[0] = "5G";
+		wanttype[1] = "LTE";
+
+		String[] wanttype2 = new String[1];
+		wanttype2[0] = "5G";
+		
+
+
+		String[] wanttype3 = new String[1];
+		wanttype3[0] = "NULL";
+
+		
+		
+		assertEquals(3,p.matchtype("5G",wanttype));
+		assertEquals(3,p.matchtype("LTE",wanttype));
+		assertEquals(0,p.matchtype("LTE",wanttype2));
+		assertEquals(3,p.matchtype("5G",wanttype2));
+		
+		assertEquals(0,p.matchtype("5G",wanttype3));
+		assertEquals(0,p.matchtype("5G",wanttype3));
+		
+		
+		
+		
+		
+		
 	
 	}
 
